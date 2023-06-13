c4bdfacf32203fb4638d5fffc6408ca250e216b9
Raskolnikov <jerry.tann@endress.com>
2023-06-13 17:11:18 +0800

    📝 full config for a log project + store + config

--- sls/exampleLogProj.tf
+++ sls/exampleLogProj.tf
@@ -21,3 +21,64 @@ resource "alicloud_log_project" "production" {
 
     timeouts {}
 }
+
+resource "alicloud_log_store" "onesubscription-staging" {
+    append_meta           = true
+    auto_split            = true
+    enable_web_tracking   = false
+    hot_ttl               = 0
+    max_split_shard_count = 64
+    mode                  = "standard"
+    name                  = "onesubscription"
+    project               = "k8s-log-ce1d89e1ffb644a42ad94a226e5e8aa52"
+    retention_period      = 180
+    shard_count           = 2
+
+
+    timeouts {}
+}
+
+resource "alicloud_logtail_config" "onesubscription-staging" {
+    input_detail = jsonencode(
+        {
+            adjustTimezone  = false
+            delayAlarmBytes = 0
+            discardNonUtf8  = false
+            enableRawLog    = false
+            enableTag       = false
+            filterKey       = []
+            filterRegex     = []
+            localStorage    = true
+            logTimezone     = ""
+            maxSendRate     = -1
+            mergeType       = "topic"
+            plugin          = {
+                global = {
+                    AlwaysOnline = true
+                    type         = "k8sStdout"
+                }
+                inputs = [
+                    {
+                        detail = {
+                            IncludeK8sLabel = {
+                                app = "onesubscription"
+                            }
+                            Stderr          = true
+                            Stdout          = true
+                        }
+                        type   = "service_docker_stdout"
+                    },
+                ]
+            }
+            priority        = 0
+            sendRateExpire  = 0
+            sensitive_keys  = []
+            shardHashKey    = []
+        }
+    )
+    input_type   = "plugin"
+    logstore     = "onesubscription"
+    name         = "kubernetes-stdout-config-ngxjnkaw"
+    output_type  = "LogService"
+    project      = "k8s-log-ce1d89e1ffb644a42ad94a226e5e8aa52"
+}
